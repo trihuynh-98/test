6964a6a7989d46c5172d3b652d928850df588377
root <choihetbuon@gmail.com>
2025-04-12 15:01:38 +0700

    update dockerfile

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+  console.log(`Server running on http://0.0.0.0:${port}/`);
 });
